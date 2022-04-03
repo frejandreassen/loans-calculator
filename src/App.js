5d6d19daec71b52b49af4c1444ad37e424bf7a96
Frej Andreassen <frej.andreassen@gmail.com>
2022-04-03 08:09:07 +0200

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import LoansPage from './Loans/LoansPage';
+import React from 'react'
+
+/* Components */
+import LoansForm from './Loans/LoansForm'
+import Grid from "@mui/material/Grid"
+import Container from "@mui/material/Container"
+
+/* Assets */
 
 function App() {
-  return (
-    <div>
-      <LoansPage />
-    </div>
-  );
+    
+    const initialValues = {
+        loans: [
+          {
+            pv: '',
+            interest: '',
+            years: '',
+            pmt: '',
+            stmt: '',
+            type: 'annuity',
+            expand: false,
+          },
+        ],
+        timePoint: 10
+      };
+
+    return (
+        <div>
+            <Container maxWidth="md">
+              <Grid container
+                direction="column"
+                >
+                    <h1>Loans calculator</h1>
+                    <p>Use this calculator to summarize multiple loans, calculate total costs, elaborate payments and project future debt.
+                    </p>
+              </Grid>
+            </Container>
+            <LoansForm initialValues={initialValues}/>
+          </div>
+    )
 }
 
-export default App;
+export default App
